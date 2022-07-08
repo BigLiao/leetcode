5c4405830528232778f81f4d881ecd348f0f6432
jesonliao <jesonliao@tencent.com>
2022-07-08 14:25:05 +0800

    solve problem

---
+++ 63.unique-paths-ii.ts
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode id=63 lang=typescript
+ *
+ * [63] Unique Paths II
+ *
+ * https://leetcode.com/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (38.52%)
+ * Likes:    5443
+ * Dislikes: 395
+ * Total Accepted:    566.1K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * You are given an m x n integer array grid. There is a robot initially
+ * located at the top-left corner (i.e., grid[0][0]). The robot tries to move
+ * to the bottom-right corner (i.e., grid[m-1][n-1]). The robot can only move
+ * either down or right at any point in time.
+ * 
+ * An obstacle and space are marked as 1 or 0 respectively in grid. A path that
+ * the robot takes cannot include any square that is an obstacle.
+ * 
+ * Return the number of possible unique paths that the robot can take to reach
+ * the bottom-right corner.
+ * 
+ * The testcases are generated so that the answer will be less than or equal to
+ * 2 * 10^9.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * Output: 2
+ * Explanation: There is one obstacle in the middle of the 3x3 grid above.
+ * There are two ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down -> Down
+ * 2. Down -> Down -> Right -> Right
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: obstacleGrid = [[0,1],[0,0]]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1 <= m, n <= 100
+ * obstacleGrid[i][j] is 0 or 1.
+ * 
+ * 
+ */
+
+// @lc code=start
+function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
+
+  // m行n列
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+
+  // [x,y,direction]
+  // direction=-1未走过,0向右走过，1向下走过
+  const stack: Array<Array<number>> = [];
+  stack.push([0, 0, -1]);
+  let result: number = 0;
+  while (stack.length > 0) {
+    const latest = stack[stack.length - 1];
+    if (latest[0] === n || latest[1] === m) {
+      // 碰壁了
+      stack.pop();
+    } else if (obstacleGrid[latest[1]][latest[0]] === 1) {
+      // 当前是障碍
+      stack.pop();
+    } else if (latest[0] === n - 1 && latest[1] === m - 1) {
+      // 到达终点
+      result += 1;
+      stack.pop();
+    } else  if (latest[2] === -1) {
+      // 向右走
+      latest[2] = 0;
+      const next = [latest[0] + 1, latest[1], -1];
+      stack.push(next);
+    } else if (latest[2] === 0) {
+      // 向下走
+      latest[2] = 1;
+      const next = [latest[0], latest[1] + 1, -1];
+      stack.push(next);
+    } else {
+      // 走完了，回退
+      stack.pop();
+    }
+  }
+
+  return result;
+};
+// @lc code=end
+
+const testCase = [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1], [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0], [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1], [0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0], [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1], [0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1], [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0], [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]];
+console.time('cost');
+const result = uniquePathsWithObstacles(testCase);
+console.timeEnd('cost');
+console.log('result', result);
+// Time Limit Exceeded!!!
+
